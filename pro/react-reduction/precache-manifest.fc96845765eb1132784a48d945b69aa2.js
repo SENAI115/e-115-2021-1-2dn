55308a01ac3484d2e1b171b691e8abe3837a15e5
Danilo Santana e Silva <danilo-santana-e-silva@users.noreply.github.com>
2021-06-14 22:41:03 -0300

    Add files via upload

---
+++ pro/react-reduction/precache-manifest.fc96845765eb1132784a48d945b69aa2.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d039e24fed2a4f4e365",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/0.dd539596.chunk.js"
+  },
+  {
+    "revision": "764cf4bf35563d6e7bed",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/css/main.dca043c5.chunk.css"
+  },
+  {
+    "revision": "764cf4bf35563d6e7bed",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/main.a0a117ad.chunk.js"
+  },
+  {
+    "revision": "f76ebce884b5a78e4a5b",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/runtime~main.a8aae1cb.js"
+  },
+  {
+    "revision": "74f8cb42a71995788b90",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/3.f960854e.chunk.js"
+  },
+  {
+    "revision": "17a29544c4b148264c03",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/4.4232440e.chunk.js"
+  },
+  {
+    "revision": "99c1392fb0413872a171",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/5.fcbf3563.chunk.js"
+  },
+  {
+    "revision": "b477e998382ea96e7ea4",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/6.377fbda5.chunk.js"
+  },
+  {
+    "revision": "3352a6e1863bdc3ecc40",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/7.e687dbe0.chunk.js"
+  },
+  {
+    "revision": "08266378b594e4cdd2fb",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/8.23265be5.chunk.js"
+  },
+  {
+    "revision": "fea0ddd6e4a532842a9c",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/9.7f64d341.chunk.js"
+  },
+  {
+    "revision": "fbe800ab222dccd17d4d",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/10.fb467949.chunk.js"
+  },
+  {
+    "revision": "9f390a959093ce3d4e99",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/11.fc352896.chunk.js"
+  },
+  {
+    "revision": "b58f20d2df5452d8d642",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/12.c99f52ff.chunk.js"
+  },
+  {
+    "revision": "92e8149384aca2ea1afe",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/13.cca6e818.chunk.js"
+  },
+  {
+    "revision": "4bd86826665eefcbafb8",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/14.0af116dc.chunk.js"
+  },
+  {
+    "revision": "a1c33f17257b4534779d",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/15.49a2e642.chunk.js"
+  },
+  {
+    "revision": "9424a73a7e0f4aaeac83",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/16.b2003c42.chunk.js"
+  },
+  {
+    "revision": "e88127113e343b41703c",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/17.087576cb.chunk.js"
+  },
+  {
+    "revision": "d39425503e44d6102e37",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/18.2549cd3e.chunk.js"
+  },
+  {
+    "revision": "f13c13b9870c06b8ba83",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/19.769b4b6c.chunk.js"
+  },
+  {
+    "revision": "499fb8259fc0e45ffc6a",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/js/20.361ca391.chunk.js"
+  },
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "b175e1c4790112fe609a0f3279e8535d",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/logo_200.b175e1c4.png"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "1f7beca5bf4cda1803cd59cd881c372d",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_1.1f7beca5.jpg"
+  },
+  {
+    "revision": "82e7c41e073394f808fd382c97e71884",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_2.82e7c41e.jpg"
+  },
+  {
+    "revision": "6e25d86d33b97ab231889fd4a38530b4",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_3.6e25d86d.jpg"
+  },
+  {
+    "revision": "fd53372585bf0ef377e97ee9dd7ea925",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_5.fd533725.jpg"
+  },
+  {
+    "revision": "c54eef50718560d1268aa8e92025972d",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/background_1920-2.c54eef50.jpg"
+  },
+  {
+    "revision": "b7f68e8afaec5bfad8fdcc44e4e4e483",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/background_1920-11.b7f68e8a.jpg"
+  },
+  {
+    "revision": "acfe7046f47d96b9cb598622e50fe610",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/background_1920-18.acfe7046.jpg"
+  },
+  {
+    "revision": "1420d15b22ed3f39abc468212d735814",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/background_640-1.1420d15b.jpg"
+  },
+  {
+    "revision": "55e47e98ef3c84cf5eb4706a4c74311a",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/background_640-3.55e47e98.jpg"
+  },
+  {
+    "revision": "1d98ff2e44d70e391ec7a49e4efdfa86",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/background_1920-5.1d98ff2e.jpg"
+  },
+  {
+    "revision": "1e0eb5503400ca7f2a65175cec9e9115",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/background_1920-6.1e0eb550.jpg"
+  },
+  {
+    "revision": "abf47fc19906e17185a37765e7594ee0",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/background_1920-7.abf47fc1.png"
+  },
+  {
+    "revision": "22fc19f2c02f7dba20864f097f0bfd20",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/background_1920-19.22fc19f2.jpg"
+  },
+  {
+    "revision": "a9a0ec6482226ea8098fc71a3fababe0",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/product_640-1.a9a0ec64.jpg"
+  },
+  {
+    "revision": "da0b0457353c6555a5ba4535597f2187",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/product_640-2.da0b0457.jpg"
+  },
+  {
+    "revision": "64ed27c9aca0306b4185b82e144cdedd",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/product_640-3.64ed27c9.jpg"
+  },
+  {
+    "revision": "2110d8cd7d70f6792b4c2d517cb7f593",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/product_640-4.2110d8cd.jpg"
+  },
+  {
+    "revision": "bf8840bd88a80c14418b238a7f1beb24",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/product_640-5.bf8840bd.jpg"
+  },
+  {
+    "revision": "f9249755162af20121af258ca1ffc578",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/product_640-6.f9249755.jpg"
+  },
+  {
+    "revision": "de88253696b81598494dd6bd5c0b087e",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_6.de882536.jpg"
+  },
+  {
+    "revision": "1dce100ffdb944226acf9db666a57319",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_7.1dce100f.jpg"
+  },
+  {
+    "revision": "a4be0bbf3a08f9de24548aa6765af93f",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_8.a4be0bbf.jpg"
+  },
+  {
+    "revision": "798c02eb53d02e11c3d8a6588cdf5033",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_9.798c02eb.jpg"
+  },
+  {
+    "revision": "56d054ffdf15ac516d7ba19fa9a33ad5",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_10.56d054ff.jpg"
+  },
+  {
+    "revision": "336faadc989ea15bbe2c0fb30e20bcd7",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_11.336faadc.jpg"
+  },
+  {
+    "revision": "1e0889afa5d1fa653fef4f8b5d3272d2",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_12.1e0889af.jpg"
+  },
+  {
+    "revision": "63265e929c9f7442ea1853d2dc19a188",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_13.63265e92.jpg"
+  },
+  {
+    "revision": "595b8cbdd2b7f27cf7d38b895a9b56c9",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/static/media/100_14.595b8cbd.jpg"
+  },
+  {
+    "revision": "49b82e843a1d087d751cf134b23e07c8",
+    "url": "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/index.html"
+  }
+];
---
+++ pro/react-reduction/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/precache-manifest.fc96845765eb1132784a48d945b69aa2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("https://senai115.github.io/e-115-2021-1-2dn/pro/react-reduction/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
